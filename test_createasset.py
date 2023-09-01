2d58eaa45994f77e308921334d4f68cfe242af46
rajeshp <rajeshsomawanshi@gmail.com>
2023-09-01 21:04:31 +0530

    removed taking data from excel

--- test_createasset.py
+++ test_createasset.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 import pytest
 from selenium.webdriver import ChromeOptions
 
-from Customerapp.dataexcel import TestOpen
+#from Customerapp.dataexcel import TestOpen
 
 chrome_options = ChromeOptions()
 
@@ -13,11 +13,11 @@ class TestExample:
     def test_page(self,mytest):
         self.driver.implicitly_wait(15)
         #time.sleep(5)
-        self.driver.find_element(By.ID, "mobile").send_keys(mytest["username"])
+        self.driver.find_element(By.ID, "mobile").send_keys("9823521834")
         #time.sleep(5)
         self.driver.find_element(By.ID, "Log In button").click()
 
-        self.driver.find_element(By.ID, "Password").send_keys(mytest["password"])
+        self.driver.find_element(By.ID, "Password").send_keys("repos1234")
         self.driver.find_element(By.ID, "Log In button").click()
 
         time.sleep(5)
@@ -25,9 +25,9 @@ class TestExample:
 
 
 
-    @pytest.fixture(params=TestOpen.test_myfun())
-    def mytest(self,request):
-        return request.param
+    # @pytest.fixture(params=TestOpen.test_myfun())
+    # def mytest(self,request):
+    #     return request.param
 
 
 
