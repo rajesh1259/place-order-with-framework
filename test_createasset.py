875c3bbd1c9b9171fa8587ca5adbbae44e260362
rajeshp <rajeshsomawanshi@gmail.com>
2023-09-02 12:19:14 +0530

    new changes

--- test_createasset.py
+++ test_createasset.py
@@ -3,21 +3,21 @@ from selenium.webdriver.common.by import By
 import pytest
 from selenium.webdriver import ChromeOptions
 
-#from Customerapp.dataexcel import TestOpen
+from Customerapp.dataexcel import TestOpen
 
 chrome_options = ChromeOptions()
 
 @pytest.mark.usefixtures("newset")
 class TestExample:
 
-    def test_page(self):
+    def test_page(self,mytest):
         self.driver.implicitly_wait(15)
         #time.sleep(5)
-        self.driver.find_element(By.ID, "mobile").send_keys("9823521834")
+        self.driver.find_element(By.ID, "mobile").send_keys(mytest["username"])
         #time.sleep(5)
         self.driver.find_element(By.ID, "Log In button").click()
 
-        self.driver.find_element(By.ID, "Password").send_keys("repos1234")
+        self.driver.find_element(By.ID, "Password").send_keys(mytest["password"])
         self.driver.find_element(By.ID, "Log In button").click()
 
         time.sleep(5)
@@ -25,9 +25,9 @@ class TestExample:
 
 
 
-    # @pytest.fixture(params=TestOpen.test_myfun())
-    # def mytest(self,request):
-    #     return request.param
+    @pytest.fixture(params=TestOpen.test_myfun())
+    def mytest(self,request):
+        return request.param
 
 
 
